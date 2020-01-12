26db7559f71035546778b25dd83fcadbe0b62dad
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-01-12 16:20:29 -0600

    basic login example

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>
-      <h1>Hi There!</h1>
-    </div>
-  `);
-});
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['test'] }));
 app.use(router);
 
 app.listen(3000, () => {
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,21 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
+// one solution to type definition file issue
 interface RequestWithBody extends Request {
   body: {
     [key: string]: string | undefined;
   };
+}
+
+function requiredAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+
+    return;
+  }
+
+  res.status(403);
+  res.send('Not permitted');
 }
 
 const router = Router();
@@ -27,11 +39,42 @@ router.get('/login', (req: Request, res: Response) => {
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  if (email) {
-    res.send(email.toUpperCase());
+  if (email && password && email === 'hi@hi.com' && password === 'password') {
+    req.session = {
+      loggedIn: true
+    };
+
+    res.redirect('/');
   } else {
-    res.send('You mus provide an email');
+    res.send('Invalid email or password');
   }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+    <div>
+      <h1>You are logged In</h1>
+      <a href="/logout">Logout</a>
+    </div>
+  `);
+  } else {
+    res.send(`
+    <div>
+      <h1>You are not logged In</h1>
+      <a href="/login">Login</a>
+    </div>
+  `);
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+router.get('/protected', requiredAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged User');
 });
 
 export { router };
